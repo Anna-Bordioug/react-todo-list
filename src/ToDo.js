da192853b363a31138b5154194c76e9b429c90ba
annabordioug <abordiou@uwaterloo.ca>
2021-12-29 23:21:05 -0500

    First Commit

---
+++ src/ToDo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function ToDo( {todo, toggleToDo}) {
+
+    function handleToDoClick() {
+        toggleToDo(todo.id)
+    }
+
+    return (
+        <div>
+            <label>
+                <input type="checkbox" checked={todo.complete} onChange={handleToDoClick} />
+                {todo.name}
+            </label>
+            
+        </div>
+    )
+}
